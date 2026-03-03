4155c371c9988f5bf8767e0bb3df4161efd8eefd
Abhinav-836 <ashutoshabhinavbachra0009@gmail.com>
2026-03-04 00:07:04 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-
-# Upgrade pip and install dependencies
-python3 -m pip install --upgrade pip
-pip install -r requirements.txt
-
-# Start FastAPI with uvicorn
-uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
