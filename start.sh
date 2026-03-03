4a4a3df8bd71fcd090acf3028991624d39852258
Abhinav-836 <ashutoshabhinavbachra0009@gmail.com>
2026-03-03 19:52:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 
 # Start FastAPI with uvicorn
-uvicorn main:app --host 0.0.0.0 --port $PORT
+uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
