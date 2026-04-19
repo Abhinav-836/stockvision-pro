8f2c3db3b8c7dd96e4c62f8c1b2dc6bd17230473
Abhinav Ashutosh <ashutoshabhinavbachra0009@gmail.com>
2026-04-19 19:49:39 +0530

    Change ALLOWED_ORIGINS to allow all origins

--- backend/main.py
+++ backend/main.py
@@ -105,7 +105,7 @@ class Config:
     MIN_COMPARISON_STOCKS   = int(os.getenv("MIN_COMPARISON_STOCKS", 2))
     ALLOWED_ORIGINS         = os.getenv(
         "ALLOWED_ORIGINS",
-        "http://localhost:5173,http://localhost:3000"
+        "*"
     ).split(",")
     REDIS_HOST  = os.getenv("REDIS_HOST", "localhost")
     REDIS_PORT  = int(os.getenv("REDIS_PORT", 6379))
